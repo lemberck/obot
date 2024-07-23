d24ba4b14c871b0f72119e10eeaf6db4433c098f
lemberck <blemberck@hotmail.com>
2024-07-23 12:07:16 -0300

    fix import

--- v2/src/main.py
+++ v2/src/main.py
@@ -1,10 +1,11 @@
-from gradio_interface import demo
 from loguru import logger
 from dotenv import load_dotenv 
 import os
 
 load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env'))
 logger.add("logs/v2_app.log", rotation="1 day", retention="1 week", compression="zip")
+
+from gradio_interface import demo
 
 if __name__ == "__main__":
     demo.launch(share=True)
