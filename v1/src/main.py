6d0d0f3612c659cb508ad94d87db7352b5388fa4
lemberck <blemberck@hotmail.com>
2024-07-23 12:08:17 -0300

    import fix

--- v1/src/main.py
+++ v1/src/main.py
@@ -1,4 +1,3 @@
-from gradio_interface import launch_interface
 import os
 from dotenv import load_dotenv
 from loguru import logger
@@ -6,5 +5,6 @@ from loguru import logger
 load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env'))
 logger.add("logs/v1_app.log", rotation="1 day", retention="1 week", compression="zip")
 
+from gradio_interface import launch_interface
 # Launch the interface
 launch_interface()
